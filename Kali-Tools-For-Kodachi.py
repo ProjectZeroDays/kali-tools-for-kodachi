cca7dca09af6512e089af35522f07698be3637ac
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2024-07-17 07:11:04 -0700

    Create Kali-Tools-For-Kodachi.py

---
+++ Kali-Tools-For-Kodachi.py
@@ -0,0 +1,12 @@
+#!/bin/bash/
+# . _____________________________________________ .
+# |                                               |
+# |                                               |
+# |      Kali Linux Tools For Kodachi Linux       |
+# |                                               |
+# |   The Ultimate Hacking & Privacy Experience   |
+# |                                               |
+# |         Project Zero - Ezra (OMG LOLz)        |
+# |             github/projectzerodays            |
+# |                                               |
+# | _____________________________________________ |
